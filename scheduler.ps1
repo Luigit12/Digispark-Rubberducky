23f7b6f2cb081a700f1c5c9b9c8a543e0c1e5c02
luigit12 <58264257+Luigit12@users.noreply.github.com>
2021-03-10 10:58:34 +0100

    Update scheduler.ps1

--- scheduler.ps1
+++ scheduler.ps1
@@ -1,4 +1,4 @@
 $settings = New-ScheduledTaskSettingsSet -Hidden
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "Start-Process https://youtu.be/dQw4w9WgXcQ"
 $trigger = New-ScheduledTaskTrigger -Daily -At 11:00
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName 'a' -Settings $settings
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName 'b' -Settings $settings
