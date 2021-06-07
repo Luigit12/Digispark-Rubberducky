1b6c7034465d7fc012ce55a0c8ac72e36122750b
luigit12 <58264257+Luigit12@users.noreply.github.com>
2021-06-07 11:43:40 +0200

    Update scheduler.ps1

--- scheduler.ps1
+++ scheduler.ps1
@@ -1,9 +1,9 @@
 
-Function Set-Speaker($Volume){$wshShell = new-object -com wscript.shell;1..50 | % {$wshShell.SendKeys([char]174)};1..50 | % {$wshShell.SendKeys([char]175)}}
+Function Set-Speaker($Volume){$wshShell = new-object -com wscript.shell;1..50 | % {$wshShell.SendKeys([char]174)};1..$Volume | % {$wshShell.SendKeys([char]175)}}
 
 Set-Alias setspeaker Set-Speaker
 
-setspeaker
+setspeaker -volume 50
 <#
 $settings = New-ScheduledTaskSettingsSet -Hidden
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "Start-Process https://Luigit12.github.io"
