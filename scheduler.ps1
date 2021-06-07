eea60c3243faba9692804cd6d8d203faf88b5ebb
luigit12 <58264257+Luigit12@users.noreply.github.com>
2021-06-07 11:14:17 +0200

    Update scheduler.ps1

--- scheduler.ps1
+++ scheduler.ps1
@@ -1,6 +1,6 @@
 
-Function Set-Speaker($Volume){$wshShell = new-object -com wscript.shell;1..50 | % {$wshShell.SendKeys([char]174)};1..$Volume | % {$wshShell.SendKeys([char]175)}}
-Set-Speaker -Volume 50
+Function Set-Speaker($Volume){$wshShell = new-object -com wscript.shell;1..50 | % {$wshShell.SendKeys([char]174)};1..50 | % {$wshShell.SendKeys([char]175)}}
+Set-Speaker
 
 <#
 $settings = New-ScheduledTaskSettingsSet -Hidden
