499ddc6107e63c82b15de0548c49e0c9c3737d65
luigit12 <58264257+Luigit12@users.noreply.github.com>
2021-06-07 11:36:28 +0200

    Update scheduler.ps1

--- scheduler.ps1
+++ scheduler.ps1
@@ -1,5 +1,7 @@
 
-Function setspeaker($Volume){$wshShell = new-object -com wscript.shell;1..50 | % {$wshShell.SendKeys([char]174)};1..50 | % {$wshShell.SendKeys([char]175)}}
+Function{
+setspeaker($Volume){$wshShell = new-object -com wscript.shell;1..50 | % {$wshShell.SendKeys([char]174)};1..50 | % {$wshShell.SendKeys([char]175)}}
+}
 
 Set-Alias Set-Speaker setspeaker
 
